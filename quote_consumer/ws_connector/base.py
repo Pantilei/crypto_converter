399cfab25eac002e8e790a36761a9f7f4b449401
Pantilei Ianulov <pantelei.i@darqube.com>
2025-10-05 23:53:27 +0300

    [SL-0006] Update real-time provider class

--- quote_consumer/ws_connector/base.py
+++ quote_consumer/ws_connector/base.py
@@ -1,4 +1,5 @@
 import asyncio
+import traceback
 from asyncio import Queue
 from collections.abc import Iterable
 from typing import TYPE_CHECKING, Any, ClassVar, get_args
@@ -22,7 +23,6 @@ class RTTradesProvider[T: BaseTradePayload]:
         __payload_type__: type[T]
         ws_url: ClassVar[str]
 
-    __ws_client__: ClassVar[WebSocketClient] = WebSocketClient()
     __trade_providers__: ClassVar[list[type["RTTradesProvider"]]] = []
     __trades_queue__: ClassVar[Queue[Trade]] = Queue(maxsize=1_000)
     _connection_retry_period: ClassVar[int] = 10
@@ -58,7 +58,8 @@ class RTTradesProvider[T: BaseTradePayload]:
 
     async def robust_listen(self) -> None:
         for sub_msgs, delay in await self.get_conn_sub_message():
-            await self._connect_and_listen(sub_msgs, delay)
+            self._listeners.append(asyncio.create_task(self._connect_and_listen(sub_msgs, delay)))
+            await asyncio.sleep(5 * (delay or 0.2))  # Add some delay between creating the connection from same IP
 
     async def stop(self) -> None:
         for listener in self._listeners:
@@ -69,32 +70,29 @@ class RTTradesProvider[T: BaseTradePayload]:
                 await conn.close()
 
     async def _connect_and_listen(
-        self,
-        sub_msgs: list[dict[str, Any]],
-        sub_msg_delay: float | None = None
+        self, sub_msgs: list[dict[str, Any]], sub_msg_delay: float | None = None
     ) -> None:
-        logger.info(f"Connecting to WS of {self.__class__.__name__}")
-        conn = await self.__ws_client__.connect(self.ws_url)
-        self._connections.append(conn)
-        listener = asyncio.create_task(self._listen(conn, sub_msgs, sub_msg_delay=sub_msg_delay))
-        self._listeners.append(listener)
+        while True:
+            logger.info(f"Connecting to WS of {self.__class__.__name__}")
+            try:
+                conn = await WebSocketClient().connect(self.ws_url)
+            except Exception as ex:
+                logger.warning(f"Connection to {self.__class__.__name__} cannot be established: {ex}")
+                await asyncio.sleep(self._connection_retry_period)
+                continue
 
-        def __retry_callback(fut: asyncio.Future[None]) -> None:
-            if fut.cancelled():
-                return
-            if ex := fut.exception():
-                logger.critical(f"Error on listening: {ex}")
-                return
-            self._connections = [c for c in self._connections if c is not conn]
-            self._listeners.remove(listener)
-            asyncio.get_running_loop().call_later(
-                self._connection_retry_period,
-                lambda s_msgs=sub_msgs: asyncio.create_task(  # type: ignore
-                    self._connect_and_listen(s_msgs, sub_msg_delay=sub_msg_delay)
-                )
-            )
+            self._connections.append(conn)
 
-        listener.add_done_callback(__retry_callback)
+            try:
+                await self._listen(conn, sub_msgs, sub_msg_delay)
+            except Exception as ex:
+                logger.critical(f"Fatal error on listen: {ex}")
+                logger.error(traceback.format_exc())
+                break
+
+            self._connections = [conn for conn in self._connections if conn.connected]
+            logger.info(f"Connection to {self.__class__.__name__} closed.")
+            await asyncio.sleep(self._connection_retry_period)
 
     async def _listen(
         self,
@@ -116,5 +114,3 @@ class RTTradesProvider[T: BaseTradePayload]:
                 logger.debug(f"Non trade message: {msg.data}")
                 continue
             await self.__trades_queue__.put(payload.to_trade())
-
-        logger.info("Connection closed")
